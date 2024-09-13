03e86ecd8adfd8ac2f688005fee0195f03b10a1b
kalrit718 <kalindu.rithmal.other@gmail.com>
2024-09-14 00:31:44 +0530

    feat: configure routing and initial settings

---
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,34 @@
+// import React from 'react';
+import './NavigationBar.css';
+import { Link } from 'react-router-dom';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+
+export default function NavigationBar() {
+  
+  return (
+    <Navbar collapseOnSelect style={{ height: "10vh" }} bg='dark' variant='dark'>
+      <Container>
+        <Navbar.Brand href="#home">
+          <span className='main-logo-label'>
+            MyTDL
+          </span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav style={{ width: "30%" }} className="me-auto"></Nav>
+          <Nav style={{ width: "70%" }} justify>
+            <Nav.Link as={Link} to='/Home' className='nav-router-link'>
+              Home Page
+            </Nav.Link>
+            <Nav.Link as={Link} to='/About' className='nav-router-link'>
+              About MyTDL
+            </Nav.Link>
+            <Nav.Link as={Link} to='/Account' className='nav-router-link'>
+              Account
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
