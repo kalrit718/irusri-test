33d30f1fc6f2abf180eaa0c689bb04b12247fa38
kalrit718 <kalindu.rithmal.other@gmail.com>
2024-09-15 03:04:04 +0530

    feat: implement the form validations

--- src/components/AddTaskButton/AddTaskButton.tsx
+++ src/components/AddTaskButton/AddTaskButton.tsx
@@ -1,7 +1,16 @@
 import './AddTaskButton.css';
 import { useState } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
 import { useTodo } from '@hooks/useTodo';
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string()
+    .required('Required!'),
+  task: Yup.string()
+    .required('Required!')
+});
 
 export default function AddTaskButton() {
 
@@ -10,42 +19,86 @@ export default function AddTaskButton() {
 
   const handleClose = () => setShowForm(false);
   const handleShow = () => setShowForm(true);
-  
-  const onSubmit = () => {
-    const newTaskTitle: string = (document.getElementById('new-task-title') as HTMLInputElement).value;
-    const newTask: string = (document.getElementById('new-task') as HTMLTextAreaElement).value;
-    
-    addTodo(newTaskTitle, newTask);
-    handleClose();
-  }
 
   return (
     <>
       <Button variant='secondary' onClick={handleShow}>Add Task</Button>
       <Modal show={showForm} onHide={handleClose} centered>
-        <Form>
-          <Modal.Header closeButton>
-            <Modal.Title>Add New Task</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-              <Form.Group className='mb-3' controlId='new-task-title'>
-                <Form.Label>ToDo Task Title</Form.Label>
-                <Form.Control type='text' />
-              </Form.Group>
-              <Form.Group className='mb-3' controlId='new-task'>
-                <Form.Label>ToDo Task</Form.Label>
-                <Form.Control as='textarea' rows={3} />
-              </Form.Group>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant='muted' onClick={handleClose}>
-              Discard
-            </Button>
-            <Button variant='secondary' onClick={onSubmit}>
-              Save Task
-            </Button>
-          </Modal.Footer>
-        </Form>
+        <Formik 
+          initialValues={{
+            title: '',
+            task: ''
+          }}
+          validationSchema={validationSchema} 
+          onSubmit={(values, { setSubmitting }) => {
+            addTodo(values.title, values.task);
+            setSubmitting(false);
+            handleClose();
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting
+          }) => (
+            <Form onSubmit={handleSubmit}>
+              <Modal.Header closeButton>
+                <Modal.Title>Add New Task</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                  <Form.Group className='mb-3' controlId='new-task-title'>
+                    <Form.Label>ToDo Task Title</Form.Label>
+                    <Form.Control 
+                      type='text' 
+                      name='title' 
+                      onChange={handleChange} 
+                      onBlur={handleBlur} 
+                      value={values.title} 
+                      isInvalid={touched.title && errors.title !== ''} 
+                    />
+                    {
+                      touched.title && errors.title && (
+                        <Form.Control.Feedback type='invalid'>
+                          {errors.title}
+                        </Form.Control.Feedback>
+                      )
+                    }
+                  </Form.Group>
+                  <Form.Group className='mb-3' controlId='new-task'>
+                    <Form.Label>ToDo Task</Form.Label>
+                    <Form.Control 
+                      as='textarea' 
+                      rows={3} 
+                      name='task' 
+                      onChange={handleChange} 
+                      onBlur={handleBlur} 
+                      value={values.task} 
+                      isInvalid={touched.task && errors.task !== ''} 
+                    />
+                    {
+                      touched.task && errors.task && (
+                        <Form.Control.Feedback type='invalid'>
+                          {errors.task}
+                        </Form.Control.Feedback>
+                      )
+                    }
+                  </Form.Group>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant='muted' onClick={handleClose}>
+                  Discard
+                </Button>
+                <Button variant='secondary' type='submit' disabled={isSubmitting}>
+                  Save Task
+                </Button>
+              </Modal.Footer>
+            </Form>
+          )}
+        </Formik>
       </Modal>
     </>
   );
--- src/components/EditTodoCardButton/EditTodoCardButton.tsx
+++ src/components/EditTodoCardButton/EditTodoCardButton.tsx
@@ -1,6 +1,8 @@
 import './EditTodoCardButton.css';
 import { useState } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
 import { Todo } from '@context/TodoContext';
 import { useTodo } from '@hooks/useTodo';
 import EditButton from '/src/assets/edit-button.svg'; 
@@ -9,6 +11,13 @@ interface EditTodoCardButtonProps {
   todo: Todo
 }
 
+const validationSchema = Yup.object().shape({
+  title: Yup.string()
+    .required('Required!'),
+  task: Yup.string()
+    .required('Required!')
+});
+
 export default function EditTodoCardButton({ todo }: EditTodoCardButtonProps) {
 
   const [showForm, setShowForm] = useState(false);
@@ -16,14 +25,6 @@ export default function EditTodoCardButton({ todo }: EditTodoCardButtonProps) {
 
   const handleClose = () => setShowForm(false);
   const handleShow = () => setShowForm(true);
-  
-  const onSubmit = () => {
-    const newTaskTitle: string = (document.getElementById('new-task-title') as HTMLInputElement).value;
-    const newTask: string = (document.getElementById('new-task') as HTMLTextAreaElement).value;
-    
-    editTodo({ id: todo.id, title: newTaskTitle, task: newTask });
-    handleClose();
-  }
 
   return (
     <>
@@ -31,29 +32,81 @@ export default function EditTodoCardButton({ todo }: EditTodoCardButtonProps) {
         <img src={EditButton} className='edit-button' title='Edit' />
       </div>
       <Modal show={showForm} onHide={handleClose} centered>
-        <Form>
-          <Modal.Header closeButton>
-            <Modal.Title>Edit Task</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-              <Form.Group className='mb-3' controlId='new-task-title'>
-                <Form.Label>ToDo Task Title</Form.Label>
-                <Form.Control type='text' defaultValue={todo.title} />
-              </Form.Group>
-              <Form.Group className='mb-3' controlId='new-task'>
-                <Form.Label>ToDo Task</Form.Label>
-                <Form.Control as='textarea' rows={3} defaultValue={todo.task} />
-              </Form.Group>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant='muted' onClick={handleClose}>
-              Discard
-            </Button>
-            <Button variant='secondary' onClick={onSubmit}>
-              Save Task
-            </Button>
-          </Modal.Footer>
-        </Form>
+        <Formik 
+          initialValues={{
+            title: todo.title,
+            task: todo.task
+          }}
+          validationSchema={validationSchema} 
+          onSubmit={(values, { setSubmitting }) => {
+            editTodo({ id: todo.id, title: values.title, task: values.task });
+            setSubmitting(false);
+            handleClose();
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting
+          }) => (
+            <Form onSubmit={handleSubmit}>
+              <Modal.Header closeButton>
+                <Modal.Title>Edit Task</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                  <Form.Group className='mb-3' controlId='new-task-title'>
+                    <Form.Label>ToDo Task Title</Form.Label>
+                    <Form.Control 
+                      type='text' 
+                      name='title' 
+                      onChange={handleChange} 
+                      onBlur={handleBlur} 
+                      value={values.title} 
+                      isInvalid={touched.title && errors.title !== ''} 
+                    />
+                    {
+                      touched.title && errors.title && (
+                        <Form.Control.Feedback type='invalid'>
+                          {errors.title}
+                        </Form.Control.Feedback>
+                      )
+                    }
+                  </Form.Group>
+                  <Form.Group className='mb-3' controlId='new-task'>
+                    <Form.Label>ToDo Task</Form.Label>
+                    <Form.Control 
+                      as='textarea' 
+                      rows={3} 
+                      name='task' 
+                      onChange={handleChange} 
+                      onBlur={handleBlur} 
+                      value={values.task} 
+                      isInvalid={touched.task && errors.task !== ''} 
+                    />
+                    {
+                      touched.task && errors.task && (
+                        <Form.Control.Feedback type='invalid'>
+                          {errors.task}
+                        </Form.Control.Feedback>
+                      )
+                    }
+                  </Form.Group>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant='muted' onClick={handleClose}>
+                  Discard
+                </Button>
+                <Button variant='secondary' type='submit' disabled={isSubmitting}>
+                  Save Task
+                </Button>
+              </Modal.Footer>
+            </Form>
+          )}
+        </Formik>
       </Modal>
     </>
   );
